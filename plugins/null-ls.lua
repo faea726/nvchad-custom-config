db586348f849cffe0b1c13c606b86e4f0acb8a6c
faea726 <fantasy.eagle96@gmail.com>
2022-12-06 17:43:32 +0700

    format on save

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -41,7 +41,23 @@ local sources = {
   b.diagnostics.solhint,
 }
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup {
   debug = true,
   sources = sources,
+
+  -- Format on save
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.formatting_sync()
+        end,
+      })
+    end
+  end,
 }
