b3874dfb683b9ffaf91929214c9f1661d9b453b7
faea726 <fantasy.eagle96@gmail.com>
2022-12-09 17:19:04 +0700

    fix decrypted function

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -55,7 +55,7 @@ null_ls.setup {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.formatting_sync()
+          vim.lsp.buf.format()
         end,
       })
     end
